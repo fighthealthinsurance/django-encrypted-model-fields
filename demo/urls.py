525e47791d2590f3649fdb2e1d3aebe15f2ce65f
Holden Karau <holden@pigscanfly.ca>
2025-01-12 21:01:06 -0800

    Upgrade to django 5

--- demo/urls.py
+++ demo/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.contrib import admin
 
 from django_encrypted_filefield.constants import FETCH_URL_NAME
@@ -7,10 +7,10 @@ from .views import MyDetailView, MyFetchView, MyFormView
 
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^detail/(?P<pk>\d+)$", MyDetailView.as_view(), name="detail"),
-    url(r"^$", MyFormView.as_view(), name="index"),
+    path(r"admin/", admin.site.urls),
+    re_path(r"^detail/(?P<pk>\d+)$", MyDetailView.as_view(), name="detail"),
+    path("", MyFormView.as_view(), name="index"),
     # This URL has to exist somewhere with this pattern and this name.  The
     # view used is up to you.
-    url(r"^fetch/(?P<path>.+)", MyFetchView.as_view(), name=FETCH_URL_NAME),
+    re_path(r"^fetch/(?P<path>.+)", MyFetchView.as_view(), name=FETCH_URL_NAME)
 ]
